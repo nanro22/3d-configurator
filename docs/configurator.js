f4f9aec804cd84eeb76c05dd56193dbb3b7da56e
sebastien michea <smichea@gmail.com>
2020-08-14 07:57:52 +0800

    add transparent mode

--- docs/configurator.js
+++ docs/configurator.js
@@ -6,3 +6,13 @@ document.getElementById("toggleHat").addEventListener("click",(elm,event)=>{
     console.log("clicked toggleHat");
     unityInstance.SendMessage("ApiManager","setParameter","hat:change");
 })
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            document.getElementById('imgback').src=e.target.result;
+            document.getElementById('unityContainer').style.backgroundImage="url('" + document.getElementById('imgback').src + "')";
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+}
